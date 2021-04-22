fb7385eed119c4ac42cf9472b5e363c02158f996
quanth0171998 <thoangquant@gmail.com>
2021-04-22 23:09:01 +0700

    update pending order
    update pending order

--- src/main/java/com/datn2021/controller/UserController.java
+++ src/main/java/com/datn2021/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.datn2021.controller;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.datn2021.model.Sales;
 import com.datn2021.model.User;
 import com.datn2021.repo.OrderFinalRepository;
 import com.datn2021.repo.UserRepository;
@@ -21,6 +26,25 @@ import com.datn2021.repo.UserRepository;
 @RequestMapping("/user")
 public class UserController {
 	@Autowired private UserRepository repo;
+	
+	@GetMapping("")
+	public List<User> getListUser() {
+		return repo.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<User> getUserById(@PathVariable Long id){
+		return repo.findById(id);
+	}
+	
+	@PostMapping("")
+	public User createUser(@RequestBody User newUser){
+		if(newUser.getId()==null) {
+			newUser.setId(new Long(0));
+		}
+		return repo.save(newUser);
+	
+	}
 	
 	@PostMapping("/getUser")
 	public User getUser(@RequestBody Map<String, String> userdata){
